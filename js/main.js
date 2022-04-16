6fb421066670b3aea279a3089ebfa71128335c7f
Adam <96756923+ramblingadam@users.noreply.github.com>
2022-04-16 02:20:42 -0500

    added license

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
 
+
 // ***Grab HTML Elements***
 // Buttons:
 leftBtn = document.querySelector('#leftBtn')
 rightBtn = document.querySelector('#rightBtn')
 
 // Output Fields:
-
-
 titleJP = document.querySelector('#titleJP')
 titleRomanized = document.querySelector('#titleRomanized')
 title = document.querySelector('#title')
@@ -21,11 +20,7 @@ runtime = document.querySelector('#runtime')
 description = document.querySelector('#description')
 
 
-html = document.querySelector('html')
-
-
 // ***Event Listeners***
-
 leftBtn.addEventListener('click', function() {browseLeft()})
 rightBtn.addEventListener('click', function() {browseRight()})
 
@@ -53,8 +48,8 @@ function browseLeft() {
     music.play()
     document.querySelector('.titleBox').classList.remove('hidden')
     document.querySelector('.detailsTable').classList.remove('hidden')
-    //If first click, or at end of array, set movie to be displayed to 0
-    if(browseIndex === undefined || browseIndex <= 0) browseIndex = 21 
+    //If first click, or at end of array, set movie to be displayed to last in movies array
+    if(browseIndex === undefined || browseIndex <= 0) browseIndex = movies.length - 1 
     else browseIndex -= 1
     updateDisplay()   
 }
@@ -63,7 +58,6 @@ function browseRight() {
     music.play()
     document.querySelector('.titleBox').classList.remove('hidden')
     document.querySelector('.detailsTable').classList.remove('hidden')
-
     //If first click, or at end of array, set movie to be displayed to 0
     if(browseIndex === undefined || browseIndex >= movies.length - 1) browseIndex = 0 
     else browseIndex += 1
@@ -75,12 +69,6 @@ function updateDisplay() {
     titleRomanized.innerText = movies[browseIndex].original_title_romanised
     title.innerText = movies[browseIndex].title
     poster.src = movies[browseIndex].image
-
-    // year.innerText = `Year: ${movies[browseIndex].release_date}`
-    // director.innerText = `Director: ${movies[browseIndex].director}`
-    // producer.innerText = `Producer: ${movies[browseIndex].producer}`
-    // rtScore.innerText = `RT Score: ${movies[browseIndex].rt_score}`
-    // runtime.innerText = `Runtime: ${movies[browseIndex].running_time}`
     
     year.innerText = movies[browseIndex].release_date
     director.innerText = movies[browseIndex].director
@@ -92,8 +80,6 @@ function updateDisplay() {
     runtime.innerText = `${movies[browseIndex].running_time} minutes`
 
     description.innerText = movies[browseIndex].description
-
-
 }
 
 
